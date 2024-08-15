d7b8a1c842529720cd3ae8aef33b68c2a5a73ed7
Raneem28 <sultanraneem6g1@gmail.com>
2024-08-15 15:25:09 +0500

    useref

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // App.js
-import React from "react";
+import React, {useRef} from "react";
 import { NavTabs } from "./components/navtabs";
 import { Welcome } from "./components/welcome";
 import { Pricing } from "./components/pricing";
@@ -13,19 +13,33 @@ import { Subscribe } from "./components/Subscribe";
 import { Ending } from "./components/Ending";
 import "./App.css";
 function App() {
+  const homeRef = useRef(null);
+  const AboutRef = useRef(null);
+  const ServicesRef = useRef(null);
+  const OurTeamRef=useRef(null);
+  const contactRef=useRef(null);
   return (
     <div className="bd">
-      <NavTabs />
-      <Welcome/>
-      <Tasks/>
-      <Aboutus/>
-      <Team/>
+      <NavTabs 
+      homeRef={homeRef}
+      AboutRef={AboutRef}
+      ServicesRef={ServicesRef}
+      OurTeamRef={OurTeamRef}
+      contactRef={contactRef}/>
+      <div ref={homeRef}>
+        <Welcome />
+      </div>
+      <div ref={ServicesRef}><Tasks/></div>
+      <div ref={AboutRef}>
+        <Aboutus />
+      </div>
+      <div ref={OurTeamRef}><Team/></div>
       <Portfolio/>
       <Testimonials />
       <Pricing />
       <Images/>
       <Subscribe/>
-      <Ending/>
+      <div ref={contactRef}><Ending/></div>
     </div>
   );
 }
--- src/components/navtabs.js
+++ src/components/navtabs.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
-import { Nav_data } from "./content";
 
-export function NavTabs() {
+export function NavTabs({ homeRef, AboutRef, ServicesRef,OurTeamRef,contactRef }) {
   const [selectedTab, setSelectedTab] = useState("");
   const [activeTabs, setActiveTabs] = useState(false);
-  const handleClick = (title) => {
-    setSelectedTab(title);
+
+  const scrollToSection = (ref) => {
+    ref.current.scrollIntoView({ behavior: "smooth" });
   };
-  const handlenavbar=() =>{
-    setActiveTabs(!activeTabs)
-  }
+
+  const handleClick = (title, ref) => {
+    setSelectedTab(title);
+    scrollToSection(ref);
+  };
+
+  const handlenavbar = () => {
+    setActiveTabs(!activeTabs);
+  };
 
   return (
     <div
@@ -24,69 +30,112 @@ export function NavTabs() {
         paddingLeft: "40px",
         paddingRight: "20px",
         boxShadow: "0 2px 4px rgba(0,0,0,0.1)",
+        position:"sticky",
+        top:0,
+        zIndex:1000
       }}
     >
       <div
         style={{
           fontSize: "28px",
-          color: "#D91966"
+          color: "#D91966",
         }}
       >
         Software Company
         <button
-        className="navbutton"
-        onClick={handlenavbar}
-        style={{
-          display:"flex",
-          flexDirection:"row",
-          padding:"5px",
-          marginLeft:"20px",
-          cursor:"pointer"
-        }}>
+          className="navbutton"
+          onClick={handlenavbar}
+          style={{
+            display: "flex",
+            flexDirection: "row",
+            padding: "5px",
+            marginLeft: "20px",
+            cursor: "pointer",
+          }}
+        >
           <div className="dash"></div>
           <div className="dash"></div>
           <div className="dash"></div>
         </button>
-        <div className="small-screen"> 
-        {activeTabs && (
-        <ul className="smallul"
-          style={{
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            padding: 0,
-            margin: 0,
-            listStyle: "none",
-            gap: "30px",
-            alignItems: "center",
-            marginRight: "45px",
-        }}
-      >
-        {Nav_data.map((item, index) => (
-          <li
-            key={index}
-            onClick={() => handleClick(item.title)}
-            style={{
-              color: selectedTab === item.title ? "#D91966" : "#000000",
-              fontSize: "18px",
-              cursor: "pointer",
-            }}
-          >
-            {item.title}
-          </li>
-        ))}
-        <li>
-          <button className="started">Get Started</button>
-        </li>
-      </ul>
-        )}</div>
+        <div className="small-screen">
+          {activeTabs && (
+            <ul
+              className="smallul"
+              style={{
+                display: "flex",
+                flexDirection: "row",
+                flexWrap: "wrap",
+                padding: 0,
+                margin: 0,
+                listStyle: "none",
+                gap: "30px",
+                alignItems: "center",
+                marginRight: "45px",
+              }}
+            >
+              <li
+                onClick={() => handleClick("Home", homeRef)}
+                style={{
+                  color: selectedTab === "Home" ? "#D91966" : "#000000",
+                  fontSize: "18px",
+                  cursor: "pointer",
+                }}
+              >
+                Home
+              </li>
+              <li
+                onClick={() => handleClick("About", AboutRef)}
+                style={{
+                  color: selectedTab === "About" ? "#D91966" : "#000000",
+                  fontSize: "18px",
+                  cursor: "pointer",
+                }}
+              >
+                About
+              </li>
+              <li
+                onClick={() => handleClick("Services", ServicesRef)}
+                style={{
+                  color: selectedTab === "Services" ? "#D91966" : "#000000",
+                  fontSize: "18px",
+                  cursor: "pointer",
+                }}
+              >
+                Services
+              </li>
+              <li
+                onClick={() => handleClick("Team", OurTeamRef)}
+                style={{
+                  color: selectedTab === "Team" ? "#D91966" : "#000000",
+                  fontSize: "18px",
+                  cursor: "pointer",
+                }}
+              >
+                Team
+              </li>
+              <li
+                onClick={() => handleClick("Contact", contactRef)}
+                style={{
+                  color: selectedTab === "Contact" ? "#D91966" : "#000000",
+                  fontSize: "18px",
+                  cursor: "pointer",
+                }}
+              >
+                Contact
+              </li>
+              <li>
+                <button className="started">Get Started</button>
+              </li>
+            </ul>
+          )}
+        </div>
       </div>
       <ul
         className="ul"
         style={{
           display: "flex",
           flexDirection: "row",
-          flexWrap:"wrap",
+          flexWrap: "wrap",
           padding: 0,
           margin: 0,
           listStyle: "none",
@@ -95,19 +144,56 @@ export function NavTabs() {
           marginRight: "45px",
         }}
       >
-        {Nav_data.map((item, index) => (
-          <li
-            key={index}
-            onClick={() => handleClick(item.title)}
-            style={{
-              color: selectedTab === item.title ? "#D91966" : "#000000",
-              fontSize: "18px",
-              cursor: "pointer",
-            }}
-          >
-            {item.title}
-          </li>
-        ))}
+        <li
+          onClick={() => handleClick("Home", homeRef)}
+          style={{
+            color: selectedTab === "Home" ? "#D91966" : "#000000",
+            fontSize: "18px",
+            cursor: "pointer",
+          }}
+        >
+          Home
+        </li>
+        <li
+          onClick={() => handleClick("About", AboutRef)}
+          style={{
+            color: selectedTab === "About" ? "#D91966" : "#000000",
+            fontSize: "18px",
+            cursor: "pointer",
+          }}
+        >
+          About
+        </li>
+        <li
+          onClick={() => handleClick("Services", ServicesRef)}
+          style={{
+            color: selectedTab === "Services" ? "#D91966" : "#000000",
+            fontSize: "18px",
+            cursor: "pointer",
+          }}
+        >
+          Services
+        </li>
+        <li
+                onClick={() => handleClick("Team", OurTeamRef)}
+                style={{
+                  color: selectedTab === "Team" ? "#D91966" : "#000000",
+                  fontSize: "18px",
+                  cursor: "pointer",
+                }}
+              >
+                Team
+              </li>
+              <li
+                onClick={() => handleClick("Contact", contactRef)}
+                style={{
+                  color: selectedTab === "Contact" ? "#D91966" : "#000000",
+                  fontSize: "18px",
+                  cursor: "pointer",
+                }}
+              >
+                Contact
+              </li>
         <li>
           <button className="started">Get Started</button>
         </li>
